dd06a02475858adeceb602534c6b2cbaff022747
aiden-jung <jimmy53120488@gmail.com>
2020-08-18 12:40:39 +0900

    TS: primitive type Array 연습

--- src/practice.ts
+++ src/practice.ts
@@ -1,9 +1,14 @@
-const yunhwan: {
-    name: string,
-    age: number,
-} = {
+const yunhwan = {
     name: "yunhwan",
-    age: 29
+    age: 29,
+    hobbies: ["basketball", "game"]
 };
+
+let favoriteActivities: (string | number)[];
+favoriteActivities = ["string", 1, true];
+
+for (const hobby of yunhwan.hobbies) {
+    console.log(hobby);
+}
 
 console.log(yunhwan.name);
