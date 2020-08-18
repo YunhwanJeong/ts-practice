06d6e6590212a576d3195a0323a1efd1cdb6787f
aiden-jung <jimmy53120488@gmail.com>
2020-08-18 13:15:38 +0900

    TS: type enum 연습

--- src/practice.ts
+++ src/practice.ts
@@ -1,25 +1,12 @@
-const yunhwan: {
-    name: string,
-    age: number,
-    hobbies: string[],
-    role: [number, string]
-} = {
+enum Role {USER, ADMIN, SUPER_ADMIN};
+
+console.log(Role);
+
+const yunhwan = {
     name: "yunhwan",
     age: 29,
     hobbies: ["basketball", "game"],
-    role: [2, "admin"]
+    role: Role[2]
 };
 
-yunhwan.role.push(1); // ! Exception !
-yunhwan.role[1] = 1;
-yunhwan.role = [1, "superadmin"];
-yunhwan.role = [1, "superadmin", 11];
-
-let favoriteActivities: (string | number)[];
-favoriteActivities = ["string", 1];
-
-for (const hobby of yunhwan.hobbies) {
-    console.log(hobby);
-}
-
-console.log(yunhwan.name);
+console.log(yunhwan.role);
