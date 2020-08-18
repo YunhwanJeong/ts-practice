44dc83723b941bcda351284bc8e9d35152dfee96
aiden-jung <jimmy53120488@gmail.com>
2020-08-18 12:55:38 +0900

    TS: type TUPLE 연습

--- src/practice.ts
+++ src/practice.ts
@@ -1,11 +1,22 @@
-const yunhwan = {
+const yunhwan: {
+    name: string,
+    age: number,
+    hobbies: string[],
+    role: [number, string]
+} = {
     name: "yunhwan",
     age: 29,
-    hobbies: ["basketball", "game"]
+    hobbies: ["basketball", "game"],
+    role: [2, "admin"]
 };
 
+yunhwan.role.push(1); // ! Exception !
+yunhwan.role[1] = 1;
+yunhwan.role = [1, "superadmin"];
+yunhwan.role = [1, "superadmin", 11];
+
 let favoriteActivities: (string | number)[];
-favoriteActivities = ["string", 1, true];
+favoriteActivities = ["string", 1];
 
 for (const hobby of yunhwan.hobbies) {
     console.log(hobby);
