f07f7e6dfdb942d65043706faf247d598af52b39
aiden-jung <jimmy53120488@gmail.com>
2020-08-18 11:23:56 +0900

    TS: primitive type 연습

--- src/practice.ts
+++ src/practice.ts
@@ -1,11 +1,9 @@
-const button = document.getElementById("button")!;
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
-
-function add(num1: number, num2: number) {
-  return num1 + num2;
+function add(n1: number, n2: number) {
+    console.log(n1 + n2);
 }
 
-button.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value));
-})
+let n1: number;
+n1 = 5;
+const n2 = 10;
+
+add(n1, n2);
